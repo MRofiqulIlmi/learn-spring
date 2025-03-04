33fd7a0ec15430663aeff5ade8ba2bbf2c2c80f3
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2025-03-04 20:58:04 +0700

    add comment

--- getstart.demo/src/main/java/com/in28minutes/getstart/getstart/demo/BinarySearchImpl.java
+++ getstart.demo/src/main/java/com/in28minutes/getstart/getstart/demo/BinarySearchImpl.java
@@ -7,6 +7,10 @@ public class BinarySearchImpl {
 	
 	
 	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		//1. super in Java refers to the parent class and is used to call its constructor or methods from a child class.
+		//2. 'super()' must be first statement in constructor
+		//3. If the parent class only has a constructor with parameters, Java does not automatically create a default constructor.
+		//	In this case, you must explicitly call super(arguments) in the child constructor.
 		super();
 		this.sortAlgorithm = sortAlgorithm;
 	}
--- getstart.demo/src/main/java/com/in28minutes/getstart/getstart/demo/SortAlgorithm.java
+++ getstart.demo/src/main/java/com/in28minutes/getstart/getstart/demo/SortAlgorithm.java
@@ -1,5 +1,9 @@
 package com.in28minutes.getstart.getstart.demo;
 
+//An interface in Java is a blueprint that defines methods but does not provide implementations. Classes that implement the interface must define those methods.
+//interface is like a contract
+//also interface can create default method that can be overwritten
+//interface can provide public static final constant (cannot overwritten), also method (default or static)
 public interface SortAlgorithm {
 	public int[] sort(int[] numbers);
 }
