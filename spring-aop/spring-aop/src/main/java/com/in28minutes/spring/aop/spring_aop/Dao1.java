94c5a3a208b9a06e85a24578007a0d670578a0c1
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2025-10-15 18:47:09 +0700

    setup for learn spring aop

---
+++ spring-aop/spring-aop/src/main/java/com/in28minutes/spring/aop/spring_aop/Dao1.java
@@ -0,0 +1,12 @@
+package com.in28minutes.spring.aop.spring_aop;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Dao1 {
+
+	public String retrieveSomething() {
+		return "Dao1";
+	}
+
+}
---
+++ spring-aop/spring-aop/src/main/java/com/in28minutes/spring/aop/spring_aop/Dao2.java
@@ -0,0 +1,12 @@
+package com.in28minutes.spring.aop.spring_aop;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Dao2 {
+
+	public String retrieveSomething() {
+		return "Dao2";
+	}
+
+}
