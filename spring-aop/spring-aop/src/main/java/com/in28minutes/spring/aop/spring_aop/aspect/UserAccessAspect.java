d70b863207d1cfa86a8d156236362452529010e8
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2025-10-26 11:18:26 +0700

    define @Before advice in AOP

---
+++ spring-aop/spring-aop/src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/UserAccessAspect.java
@@ -0,0 +1,26 @@
+package com.in28minutes.spring.aop.spring_aop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class UserAccessAspect {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	// What kind of method calls I would intercept
+	// execution (* PACKAGE.*.*(..))
+
+	@Before("execution(* com.in28minutes.spring.aop.spring_aop.business.*.*(..))")
+	public void Before(JoinPoint joinPoint) {
+		// the example for check the user access
+		logger.info("Check for user access");
+		logger.info("Allowed execution for - {}", joinPoint);
+	}
+
+}
