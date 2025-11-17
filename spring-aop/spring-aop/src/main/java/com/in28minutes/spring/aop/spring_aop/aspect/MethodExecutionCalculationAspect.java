91545692a6edb279c71495cac30712b9f0752492
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2025-11-17 19:10:50 +0700

    Around Advice for do something before and after of joinPoint

---
+++ spring-aop/spring-aop/src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/MethodExecutionCalculationAspect.java
@@ -0,0 +1,25 @@
+package com.in28minutes.spring.aop.spring_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class MethodExecutionCalculationAspect {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Around(value = "execution(* com.in28minutes.spring.aop.spring_aop.business.*.*(..))")
+	public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		joinPoint.proceed();
+		long proceedTime = System.currentTimeMillis() - startTime;
+
+		logger.info("The time taken by {} is : {}", joinPoint, proceedTime);
+	}
+
+}
