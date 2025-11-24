769e1a20c493e6449b97b78f798d335c29f3db21
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2025-11-24 20:13:51 +0700

    Best Practice : Use common Pointcut Configuration

--- spring-aop/spring-aop/src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/AfterAopAspect.java
+++ spring-aop/spring-aop/src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/AfterAopAspect.java
@@ -15,17 +15,17 @@ public class AfterAopAspect {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@AfterReturning(value = "execution(* com.in28minutes.spring.aop.spring_aop.business.*.*(..))", returning = "result")
+	@AfterReturning(value = "com.in28minutes.spring.aop.spring_aop.aspect.CommonJoinPointConfig.businessLayerExecution()", returning = "result")
 	public void afterReturning(JoinPoint joinPoint, Object result) {
 		logger.info("{} returning with value : {}", joinPoint, result);
 	}
 
-	@AfterThrowing(value = "execution(* com.in28minutes.spring.aop.spring_aop.business.*.*(..))", throwing = "exception")
+	@AfterThrowing(value = "com.in28minutes.spring.aop.spring_aop.aspect.CommonJoinPointConfig.businessLayerExecution()", throwing = "exception")
 	public void afterThrowing(JoinPoint joinPoint, Object exception) {
 		logger.info("{} throwing with exception : {}", joinPoint, exception);
 	}
 
-	@After(value = "execution(* com.in28minutes.spring.aop.spring_aop.business.*.*(..))")
+	@After(value = "com.in28minutes.spring.aop.spring_aop.aspect.CommonJoinPointConfig.businessLayerExecution()")
 	public void afterThrowing(JoinPoint joinPoint) {
 		logger.info("After the execution of {}", joinPoint);
 	}
--- spring-aop/spring-aop/src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/UserAccessAspect.java
+++ spring-aop/spring-aop/src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/UserAccessAspect.java
@@ -16,11 +16,18 @@ public class UserAccessAspect {
 	// What kind of method calls I would intercept
 	// execution (* PACKAGE.*.*(..))
 
-	@Before("execution(* com.in28minutes.spring.aop.spring_aop.business.*.*(..))")
+	@Before("com.in28minutes.spring.aop.spring_aop.aspect.CommonJoinPointConfig.businessLayerExecution()")
 	public void Before(JoinPoint joinPoint) {
 		// the example for check the user access
 		logger.info("Check for user access");
 		logger.info("Allowed execution for - {}", joinPoint);
+	}
+
+	@Before("com.in28minutes.spring.aop.spring_aop.aspect.CommonJoinPointConfig.dataLayerExecution()")
+	public void BeforeDataAccess(JoinPoint joinPoint) {
+		// the example for check the user access
+		logger.info("Check for user data access");
+		logger.info("Allowed execution for this data layer - {}", joinPoint);
 	}
 
 }
