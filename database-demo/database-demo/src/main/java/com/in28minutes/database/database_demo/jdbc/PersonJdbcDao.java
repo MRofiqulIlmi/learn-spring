483cf8dddbbfa63375cceeca730514a994b7b0be
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2026-02-22 16:57:15 +0700

    create jdbcTemplate query for get all person data

---
+++ database-demo/database-demo/src/main/java/com/in28minutes/database/database_demo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,24 @@
+package com.in28minutes.database.database_demo.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.in28minutes.database.database_demo.entity.Person;
+
+@Repository
+public class PersonJdbcDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public List<Person> findAll() {
+
+		return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
+
+	}
+
+}
